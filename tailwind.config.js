583194f34b984328b217514147b59786b2363435
Dianov Evgenii <54498539+xdianovx@users.noreply.github.com>
2024-12-09 17:18:23 +0300

    asd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                serif: "Forum",
-            },
-            colors: {
-                gold: "#ae7d26",
-                blue: "#153252",
-            },
-        },
-    },
-    plugins: [],
+  content: ["./resources/**/*.blade.php"],
+
+  plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import sassGlobImports from "vite-plugin-sass-glob-import";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.scss", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    sassGlobImports(),
+    laravel({
+      input: ["resources/css/app.scss", "resources/js/app.js"],
+      refresh: true,
+    }),
+  ],
 });
