12fac9d27a06e61688f9d36765ae1e06a41530bf
Dianov Evgenii <54498539+xdianovx@users.noreply.github.com>
2024-12-09 16:20:56 +0300

    asd

--- app/Providers/ViewProvider.php
+++ app/Providers/ViewProvider.php
@@ -23,38 +23,38 @@ use App\Models\Stock;
 
 class ViewProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        // Admin
-        View::share('pages_routes', Page::$pages_routes);
-        View::share('blocks_routes', Block::$blocks_routes);
-        View::share('services_routes', Service::$services_routes);
-        View::share('blogs_routes', Blog::$blogs_routes);
-        View::share('categories_blog_routes', CategoryBlog::$categories_blog_routes);
-        View::share('news_routes', News::$news_routes);
-        View::share('prices_routes', Price::$prices_routes);
-        View::share('stocks_routes', Stock::$stocks_routes);
-        View::share('show_reels', ShowReel::$show_reels);
-        View::share('home_page_nums', HomePageNum::$home_page_nums);
-        View::share('show_reels', ShowReel::$show_reels);
-        View::share('specializations_routes', Specialization::$specializations_routes);
-        View::share('specialist_routes', Specialist::$specialist_routes);
-        View::share('document_routes', Document::$document_routes);
-        View::share('questions_routes', Question::$questions_routes);
-        View::share('modern_office_routes', ModernOffice::$modern_office_routes);
-        View::share('reviews_routes', Review::$reviews_routes);
-        //Client
-        View::share('parent_services', Service::whereNull('parent_id')->get());
-    }
+  /**
+   * Bootstrap services.
+   */
+  public function boot(): void
+  {
+    // Admin
+    View::share('pages_routes', Page::$pages_routes);
+    View::share('blocks_routes', Block::$blocks_routes);
+    View::share('services_routes', Service::$services_routes);
+    View::share('blogs_routes', Blog::$blogs_routes);
+    View::share('categories_blog_routes', CategoryBlog::$categories_blog_routes);
+    View::share('news_routes', News::$news_routes);
+    View::share('prices_routes', Price::$prices_routes);
+    View::share('stocks_routes', Stock::$stocks_routes);
+    View::share('show_reels', ShowReel::$show_reels);
+    View::share('home_page_nums', HomePageNum::$home_page_nums);
+    View::share('show_reels', ShowReel::$show_reels);
+    View::share('specializations_routes', Specialization::$specializations_routes);
+    View::share('specialist_routes', Specialist::$specialist_routes);
+    View::share('document_routes', Document::$document_routes);
+    View::share('questions_routes', Question::$questions_routes);
+    View::share('modern_office_routes', ModernOffice::$modern_office_routes);
+    View::share('reviews_routes', Review::$reviews_routes);
+    //Client
+    View::share('parent_services', Service::whereNull('parent_id')->get());
+  }
 }
