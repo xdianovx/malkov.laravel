c7326730fe5ab4c994edb7de09f46e6b514febff
Dianov Evgenii <54498539+xdianovx@users.noreply.github.com>
2024-12-09 18:23:13 +0300

    asdasd

--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -25,7 +25,7 @@
                             <p> {{ $item->date }}</p>
                             <div class="rating">
                                 @for ($i = 1; $i <= $item->rating; $i++)
-                                    <span class="star{{ $i <= $item->rating ? ' filled' : '' }}"><svg
+                                    <span class="star {{ $i <= $item->rating ? ' filled' : '' }}"><svg
                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                             stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star">
