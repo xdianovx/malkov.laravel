3e667e6631df74291812317cc662da8231dd6249
Nikita5745 <sliplive1991@gmail.com>
2024-12-23 19:27:46 +0300

    Up1012

--- app/Http/Requests/Document/UpdateRequest.php
+++ app/Http/Requests/Document/UpdateRequest.php
@@ -22,8 +22,8 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['required', 'max:70'],
-            'image' => 'nullable|image|max:200000|mimes:jpeg,png,jpg,gif,svg',
+            'title' => ['required', 'max:140'],
+            'image' => 'required|image|max:200000|mimes:jpeg,png,jpg,gif,svg',
             'description'  => ['nullable'],
         ];
     }
@@ -31,7 +31,7 @@ class UpdateRequest extends FormRequest
     {
         return [
             'title.required' => 'Поле "Заголовок" обязательно для заполнения',
-            'title.max' => 'Заголовок не может быть длиннее 70 символов',
+            'title.max' => 'Заголовок не может быть длиннее 140 символов',
             'image.max' => 'Размер изображения не может быть более 200кб',
             'image.mimes' => 'Изображение должно быть в одном из следующих форматов: jpeg, png, jpg, gif, svg',
         ];
