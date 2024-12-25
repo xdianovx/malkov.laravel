4227c39f37fa96ef785d4a4bcdbc2ef4b00ba902
Nikita5745 <sliplive1991@gmail.com>
2024-12-25 17:47:39 +0300

    Up251224

--- resources/views/components/sections/showreel.blade.php
+++ resources/views/components/sections/showreel.blade.php
@@ -1,11 +1,13 @@
 @isset($data->file)
     <section class="section showreel-section">
         <div class="showreel">
-            <video controls playsinline autoplay muted loop controls="false">
-                <source src="{{ $data->file }}" type="video/mp4">
-            </video>
-
-            {{-- <img src="{{ $data->file }}" alt=""> --}}
+            @if (str_contains($data->file, '.mp4') || str_contains($data->file, '.mov') || str_contains($data->file, '.ogg'))
+                <video controls playsinline autoplay muted loop controls="false">
+                    <source src="{{ $data->file }}" type="video/mp4">
+                </video>
+            @else
+                <img src="{{ $data->file }}" alt="logo">
+            @endif
         </div>
     </section>
 @endisset
